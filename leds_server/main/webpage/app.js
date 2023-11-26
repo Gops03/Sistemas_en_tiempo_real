c0257410bb9be098669cd740063291c4496fd527
Wilson Esteban Rodriguez Alarcon <wirodriguez@unal.edu.co>
2023-11-26 18:21:44 -0500

    leds_ server
    codigo que permite actualizar valores de intensidad de leds

---
+++ leds_server/main/webpage/app.js
@@ -0,0 +1,404 @@
+/**
+ * Add gobals here
+ */
+var seconds 	= null;
+var otaTimerVar =  null;
+var wifiConnectInterval = null;
+
+/**
+ * Initialize functions here.
+ */
+$(document).ready(function(){
+	//getUpdateStatus();
+	//startDHTSensorInterval();
+	$("#connect_wifi").on("click", function(){
+		checkCredentials();
+	}); 
+
+    // Función para el botón Verde
+    $("#Verde").on("click", function () {
+        // Agrega aquí la lógica para el botón Verde
+        //alert("Botón Verde clicado");
+		SeleccionLEDVERDE()
+    });
+
+    // Función para el botón Rojo
+    $("#Rojo").on("click", function () {
+        // Agrega aquí la lógica para el botón Rojo
+        //alert("Botón Rojo clicado");
+		SeleccionLEDROJO();
+    });
+
+    // Función para el botón Azul
+    $("#Azul").on("click", function () {
+        // Agrega aquí la lógica para el botón Azul
+        //alert("Botón Azul clicado");
+		SeleccionLEDAZUL()
+    });
+
+});   
+
+/**
+ * Gets file name and size for display on the web page.
+ */        
+function getFileInfo() 
+{
+    var x = document.getElementById("selected_file");
+    var file = x.files[0];
+
+    document.getElementById("file_info").innerHTML = "<h4>File: " + file.name + "<br>" + "Size: " + file.size + " bytes</h4>";
+}
+
+/**
+ * Handles the firmware update.
+ */
+function updateFirmware() 
+{
+    // Form Data
+    var formData = new FormData();
+    var fileSelect = document.getElementById("selected_file");
+    
+    if (fileSelect.files && fileSelect.files.length == 1) 
+	{
+        var file = fileSelect.files[0];
+        formData.set("file", file, file.name);
+        document.getElementById("ota_update_status").innerHTML = "Uploading " + file.name + ", Firmware Update in Progress...";
+
+        // Http Request
+        var request = new XMLHttpRequest();
+
+        request.upload.addEventListener("progress", updateProgress);
+        request.open('POST', "/OTAupdate");
+        request.responseType = "blob";
+        request.send(formData);
+    } 
+	else 
+	{
+        window.alert('Select A File First')
+    }
+}
+
+/**
+ * Progress on transfers from the server to the client (downloads).
+ */
+function updateProgress(oEvent) 
+{
+    if (oEvent.lengthComputable) 
+	{
+        getUpdateStatus();
+    } 
+	else 
+	{
+        window.alert('total size is unknown')
+    }
+}
+
+/**
+ * Posts the firmware udpate status.
+ */
+function getUpdateStatus() 
+{
+    var xhr = new XMLHttpRequest();
+    var requestURL = "/OTAstatus";
+    xhr.open('POST', requestURL, false);
+    xhr.send('ota_update_status');
+
+    if (xhr.readyState == 4 && xhr.status == 200) 
+	{		
+        var response = JSON.parse(xhr.responseText);
+						
+	 	document.getElementById("latest_firmware").innerHTML = response.compile_date + " - " + response.compile_time
+
+		// If flashing was complete it will return a 1, else -1
+		// A return of 0 is just for information on the Latest Firmware request
+        if (response.ota_update_status == 1) 
+		{
+    		// Set the countdown timer time
+            seconds = 10;
+            // Start the countdown timer
+            otaRebootTimer();
+        } 
+        else if (response.ota_update_status == -1)
+		{
+            document.getElementById("ota_update_status").innerHTML = "!!! Upload Error !!!";
+        }
+    }
+}
+
+/**
+ * Displays the reboot countdown.
+ */
+function otaRebootTimer() 
+{	
+    document.getElementById("ota_update_status").innerHTML = "OTA Firmware Update Complete. This page will close shortly, Rebooting in: " + seconds;
+
+    if (--seconds == 0) 
+	{
+        clearTimeout(otaTimerVar);
+        window.location.reload();
+    } 
+	else 
+	{
+        otaTimerVar = setTimeout(otaRebootTimer, 1000);
+    }
+}
+
+/**
+ * Gets DHT22 sensor temperature and humidity values for display on the web page.
+ */
+function getDHTSensorValues()
+{
+	$.getJSON('/dhtSensor.json', function(data) {
+		$("#temperature_reading").text(data["temp"]);
+		$("#humidity_reading").text(data["humidity"]);
+	});
+}
+
+/**
+ * Sets the interval for getting the updated DHT22 sensor values.
+ */
+function startDHTSensorInterval()
+{
+	setInterval(getDHTSensorValues, 5000);    
+}
+
+/**
+ * Clears the connection status interval.
+ */
+function stopWifiConnectStatusInterval()
+{
+	if (wifiConnectInterval != null)
+	{
+		clearInterval(wifiConnectInterval);
+		wifiConnectInterval = null;
+	}
+}
+
+/**
+ * Gets the WiFi connection status.
+ */
+function getWifiConnectStatus()
+{
+	var xhr = new XMLHttpRequest();
+	var requestURL = "/wifiConnectStatus";
+	xhr.open('POST', requestURL, false);
+	xhr.send('wifi_connect_status');
+	
+	if (xhr.readyState == 4 && xhr.status == 200)
+	{
+		var response = JSON.parse(xhr.responseText);
+		
+		document.getElementById("wifi_connect_status").innerHTML = "Connecting...";
+		
+		if (response.wifi_connect_status == 2)
+		{
+			document.getElementById("wifi_connect_status").innerHTML = "<h4 class='rd'>Failed to Connect. Please check your AP credentials and compatibility</h4>";
+			stopWifiConnectStatusInterval();
+		}
+		else if (response.wifi_connect_status == 3)
+		{
+			document.getElementById("wifi_connect_status").innerHTML = "<h4 class='gr'>Connection Success!</h4>";
+			stopWifiConnectStatusInterval();
+		}
+	}
+}
+
+/**
+ * Starts the interval for checking the connection status.
+ */
+function startWifiConnectStatusInterval()
+{
+	wifiConnectInterval = setInterval(getWifiConnectStatus, 2800);
+}
+
+/**
+ * Connect WiFi function called using the SSID and password entered into the text fields.
+ */
+function connectWifi()
+{
+	// Get the SSID and password
+	/*selectedSSID = $("#connect_ssid").val();
+	pwd = $("#connect_pass").val();
+	
+	$.ajax({
+		url: '/wifiConnect.json',
+		dataType: 'json',
+		method: 'POST',
+		cache: false,
+		headers: {'my-connect-ssid': selectedSSID, 'my-connect-pwd': pwd},
+		data: {'timestamp': Date.now()}
+	});
+	*/
+	selectedSSID = $("#connect_ssid").val();
+	pwd = $("#connect_pass").val();
+	
+	// Create an object to hold the data to be sent in the request body
+	var requestData = {
+	  'selectedSSID': selectedSSID,
+	  'pwd': pwd,
+	  'timestamp': Date.now()
+	};
+	
+	// Serialize the data object to JSON
+	var requestDataJSON = JSON.stringify(requestData);
+	
+	$.ajax({
+	  url: '/wifiConnect.json',
+	  dataType: 'json',
+	  method: 'POST',
+	  cache: false,
+	  data: requestDataJSON, // Send the JSON data in the request body
+	  contentType: 'application/json', // Set the content type to JSON
+	  success: function(response) {
+		// Handle the success response from the server
+		console.log(response);
+	  },
+	  error: function(xhr, status, error) {
+		// Handle errors
+		console.error(xhr.responseText);
+	  }
+	});
+
+
+	//startWifiConnectStatusInterval();
+}
+
+/**
+ * Checks credentials on connect_wifi button click.
+ */
+function checkCredentials()
+{
+	errorList = "";
+	credsOk = true;
+	
+	selectedSSID = $("#connect_ssid").val();
+	pwd = $("#connect_pass").val();
+	
+	if (selectedSSID == "")
+	{
+		errorList += "<h4 class='rd'>SSID cannot be empty!</h4>";
+		credsOk = false;
+	}
+	if (pwd == "")
+	{
+		errorList += "<h4 class='rd'>Password cannot be empty!</h4>";
+		credsOk = false;
+	}
+	
+	if (credsOk == false)
+	{
+		$("#wifi_connect_credentials_errors").html(errorList);
+	}
+	else
+	{
+		$("#wifi_connect_credentials_errors").html("");
+		connectWifi();    
+	}
+}
+
+/**
+ * Shows the WiFi password if the box is checked.
+ */
+function showPassword()
+{
+	var x = document.getElementById("connect_pass");
+	if (x.type === "password")
+	{
+		x.type = "text";
+	}
+	else
+	{
+		x.type = "password";
+	}
+}
+
+function SeleccionLEDROJO(){
+	var intensidad = document.getElementById("i2").value;
+	var lsuperior = document.getElementById("ls2").value;
+	var linferior= document.getElementById("li2").value;
+	var led=1;
+	//enviarSolicitudHTTP();
+	alert("LED ROJO");
+	
+}
+
+function SeleccionLEDVERDE(){
+	var intensidad = document.getElementById("i1").value;
+	var lsuperior = document.getElementById("ls1").value;
+	var linferior= document.getElementById("li1").value;
+	var led=2;
+	//enviarSolicitudHTTP();
+	alert("LED VERDE");
+}
+
+function SeleccionLEDAZUL(){
+	var intensidad = document.getElementById("i3").value;
+	var lsuperior = document.getElementById("ls3").value;
+	var linferior= document.getElementById("li3").value;
+	var led=3;
+	//enviarSolicitudHTTP();
+	alert("LED AZUL");
+}
+
+/*private static void enviarSolicitudHTTP() {
+    try {
+        // URL del servidor y ruta específica
+        URL url = new URL("http://tu_direccion_ip:puerto/set_rgb"); // Reemplaza con la dirección y puerto correctos
+
+        // Abrir la conexión
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+        // Configurar la conexión para el método POST
+        connection.setRequestMethod("POST");
+        connection.setDoOutput(true);
+
+        // Crear el cuerpo de la solicitud (datos JSON)
+        String jsonInputString = "{ \"lsuperior\": \"valor1\", \"linferior\": \"valor2\", \"intensidad\": \"valor3\", \"led\": \"valor4\" }";
+
+        // Enviar el cuerpo de la solicitud
+        try (OutputStream os = connection.getOutputStream()) {
+            byte[] input = jsonInputString.getBytes("utf-8");
+            os.write(input, 0, input.length);
+        }
+
+        // Obtener la respuesta del servidor
+        int responseCode = connection.getResponseCode();
+        System.out.println("Respuesta del servidor: " + responseCode);
+
+        // Cerrar la conexión
+        connection.disconnect();
+    } catch (Exception e) {
+        e.printStackTrace();
+    }
+}*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+    
+
+
